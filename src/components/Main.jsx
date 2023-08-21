7796dd90232f35092ab93a1a9a362f933fbac34c
taiarima <tai.arima@colorado.edu>
2023-08-22 02:05:02 +0300

    Fixed problem with choice being concealed.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,5 @@
 export default function Main({ children }) {
   return (
-    <main className="flex flex-col items-center mt-10 mb-40 h-screen">
-      {children}
-    </main>
+    <main className="flex flex-col items-center mt-10 mb-40">{children}</main>
   );
 }
